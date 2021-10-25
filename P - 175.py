b4a42c2b058f968d41d6c10685619dade4397708
Ebele-Abolo <77888430+Ebele-Abolo@users.noreply.github.com>
2021-10-24 21:48:33 -0500

    Add files via upload

---
+++ P - 175.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 23 08:15:28 2021
+
+@author: 13m0nD3m0n
+"""
+
+
+import matplotlib.pyplot as plt
+import psutil as p 
+app_name_dict = {}
+count = 0
+
+for process in p.process_iter():
+    count = count+1
+    if count <= 6:
+        name = process.name()
+        cpu_usage  = p.cpu_percent()
+        print('name : ', name, '--cpu usage : ', cpu_usage)
+        app_name_dict.update({name:cpu_usage})
+        
+keymax = max(app_name_dict, key=app_name_dict.get)
+print(keymax) 
+keymin = min(app_name_dict, key=app_name_dict.get)     
+print(keymin)   
+name_list=[keymax, keymin]
+print(name_list)
+       
+app = app_name_dict.values()
+max_app = max(app)
+print("maximum", max_app)
+min_app = min(app)
+print("minimum", min_app)
+max_min_list = [max_app,min_app]
+print(max_min_list)
+
+plt.figure(figsize = (15,8))
+plt.xlabel("Min-Max cpu-consumption")
+plt.ylabel("Usage")
+plt.bar(name_list, max_min_list,width=0.6
+,color= ("red","blue"))
+plt.show()
